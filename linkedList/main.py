945270fff516ab0f0f5c98c541f3281acfc81972
HashanEranga <ccmrhe@gmail.com>
2024-10-08 01:33:36 +0530

    Added sample main for linked list

--- linkedList/main.py
+++ linkedList/main.py
@@ -1,23 +1,25 @@
 from LinkedList import LinkedList
 
-list = LinkedList()
+l_list = LinkedList()
 
 # print nodes in an empty list
-list.print_nodes_list()
+l_list.print_nodes_list()
 
 # insert a node at the front of the list
-list.insert_at_front(12)
-list.insert_at_front(234)
-list.insert_at_front(345)
+l_list.insert_at_front(12)
+l_list.insert_at_front(234)
+l_list.insert_at_front(345)
 
 # print nodes in the list
-list.print_nodes_list()
+print("printing the list")
+l_list.print_nodes_list()
 
 # insert a node at the end of the list
-list.insert_at_end(232)
-list.insert_at_end(567)
-list.insert_at_end(532)
+l_list.insert_at_end(232)
+l_list.insert_at_end(567)
+l_list.insert_at_end(532)
 
 # print nodes in the list again
-list.print_nodes_list()
+print("printing the list")
+l_list.print_nodes_list()
 
