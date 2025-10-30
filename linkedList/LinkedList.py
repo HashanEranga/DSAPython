93855e533aaed0472e01b9b9cf5279a5b8b630a4
HashanEranga <ccmrhe@gmail.com>
2025-10-30 12:55:50 +0530

    feat() : linkedList method changed

--- linkedList/LinkedList.py
+++ linkedList/LinkedList.py
@@ -1,109 +1,85 @@
-from Node import Node
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
 class LinkedList:
-    # initialize a linked list
     def __init__(self):
         self.head = None
-        self.tail = None
 
-    def insert_at_front(self, value):
-        # 1. create a new node
-        new_node = Node(value)
+    def is_empty(self):
+        if self.head == None:
+            return True
+        return False
 
-        # 2. make the new node point to the current head
-        new_node.next = self.head
+    def get_length(self):
+        count = 0
+        current = self.head
+        while current:
+            count +=1
+            current = current.next
+        return count
 
-        # 3. make the new node the head
-        self.head = new_node
+    def display(self):
+        current = self.head
+        print("printing list items")
+        while current:
+            print(current.data)
+            current = current.next
+        print("end printing list items")
 
-        if self.tail is None:
-            self.tail = self.head
+    def insert_at_beginning(self, data):
+        newNode = Node(data)
+        newNode.next = self.head
+        self.head = newNode
 
-    def insert_at_end(self, value):
-        # 1. create a new node
-        new_node = Node(value)
+    def insert_at_end(self, data):
+        newNode = Node(data)
+        if self.head == None:
+            self.head = NewNode
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = newNode
 
-        # 2. if the list is empty, make the new node the head
+    def insert_at_position(self, data, index):
+        newNode = Node(data)
+        currentPos = index
+        current = self.head
+        if index > 0 and index <= self.get_length():
+            for i in range(currentPos - 1):
+                current = current.next
+            newNode.next = current.next
+            current.next = newNode
+        elif index < 0:
+            print("Error : Position cannot be negative")
+        else:
+            print("Error : Position cannot exceed the current length of list")
+
+    def delete_at_beginning(self):
+        if self.head != None:
+            self.head = self.head.next
+
+    def delete_at_end(self):
         if self.head is None:
-            self.head = new_node
-
-        # 3. otherwise, traverse the list to the last node
-        current_node = self.head
-
-        # 4. make the last node point to the new node
-        while current_node.next:
-            current_node = current_node.next
-
-        # 5. make the new node the last node
-        current_node.next = new_node
-
-    def insert_at_end_efficient(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
             return
-
-        self.tail.next = new_node
-        self.tail = new_node
-
-    def insert_at_k(self, value, k):
-        new_node = Node(value)
-        current_node = self.head
-
-        # 1. if there are no elements present add the node at first
-        if self.head is None:
-            self.head = new_node
+        if self.head.next is None:
+            self.head = None
             return
+        current = self.head
+        while current.next.next:
+            current = current.next
+        current.next = None
 
-        # 2. traverse till the kth position
-        for _ in range(k-1):
-            current_node = current_node.next
-
-        # 3.
-        new_node.next = current_node.next
-
-        # 4.
-        current_node.next = new_node
-
-
-
-
-    def get_head_value(self):
-        # 1. if the list is not empty, return the value of the head
-        if self.head is not None:
-            return self.head.value
-        # 2. otherwise, return -1
+    def delete_at_position(self, index):
+        current = self.head
+        if index > 0 and index <= self.get_length():
+            for i in range(index - 1):
+                current = current.next
+            if current != None:
+                current.next = current.next.next
+        elif index < 0:
+            print("Error : Position cannot be negative")
         else:
-            return -1
-
-    def get_end_value(self):
-        # 1. if the list is empty, return
-        if self.head is None:
-            return -1
-        # 2. otherwise, traverse the list to the last node
-        else:
-            current_node = self.head
-
-            while current_node.next:
-                current_node = current_node.next
-
-            return current_node.value
-
-    def print_nodes_list(self):
-        # if the list is empty, return
-        if self.head is None:
-            print("linked list is empty")
-        # otherwise, traverse the list and print the value of each node
-        else:
-            current_node = self.head
-
-            while current_node:
-                print(f"Node value : {str(current_node.value)}")
-                current_node = current_node.next
-
-
-
-
-
-
+            print("Error : Position cannot exceed the current length of list")
